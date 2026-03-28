6415a9ba95274c9c58f33a0f525f527b436ffef6
shawon miazi <shawonmiazi655@gmail.com>
2026-03-28 23:13:01 +0600

    Number theroy

---
+++ codeforces/1100/A_Sum_of_Odd_Integers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int n, k;
+        cin >> n >> k;
+        if ((n % 2 != k % 2) || ((k * k) > n))
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+}
