30ca7985abcba41269dacb64087523ed14eb841a
shawon miazi <shawonmiazi655@gmail.com>
2026-04-08 01:19:45 +0600

    constructive algorithms

---
+++ codeforces/1100/B_Collecting_Game.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int n, x=0, cnt = 0, presum = 0, sum;
+        cin >> n;
+        vector<long long > ar(n), prfsum(n,0);
+        pair<int, int> arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i].first;
+            arr[i].second = i;
+        }
+        sort(arr, arr + n);
+        for (int i = 0; i < n; i++)
+        {
+            x += arr[i].first;
+            prfsum[i] += x;
+            // cout<<x<<" ";
+        }
+        // cout<<"-";
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (i == n - 1)
+            {
+                ar[arr[i].second] = i;
+            }
+            else
+            {
+                if (prfsum[i] >= arr[i + 1].first)
+                    ar[arr[i].second] = ar[arr[i + 1].second];
+                else
+                    ar[arr[i].second] = i;
+            }
+        }
+        // for (int i = 0; i < n; i++)
+        // {
+        //     cnt = 0;
+        //     presum += arr[i].first;
+        //     sum = presum;
+        //     for (int j = i + 1; j < n; j++)
+        //     {
+        //         if (sum >= arr[j].first)
+        //         {
+        //             cnt++;
+        //             sum += arr[j].first;
+        //         }
+        //         else
+        //             break;
+        //     }
+        //     ar[arr[i].second] = i + cnt;
+        // }
+        for (int i = 0; i < n; i++)
+            cout << ar[i] << " ";
+        cout << endl;
+    }
+}
