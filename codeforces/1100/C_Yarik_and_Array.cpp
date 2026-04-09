d80720b0172124c07923c87dfa4613b30f32ff55
shawon miazi <shawonmiazi655@gmail.com>
2026-04-09 20:44:12 +0600

    Kadane's algorithm

--- codeforces/1100/C_Yarik_and_Array.cpp
+++ codeforces/1100/C_Yarik_and_Array.cpp
@@ -1,40 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
-        long long int n,sum=0,ans=0,x=0;
-        cin>>n;
-        vector<long long int>arr(n);
-        for (int i=0;i<n;i++)
+        long long int n, sum = 0, ans = 0, x = 0;
+        cin >> n;
+        vector<long long int> arr(n);
+        for (int i = 0; i < n; i++)
         {
-            cin>>arr[i];
+            cin >> arr[i];
         }
-        sum=arr[0];
-        x=arr[0];
-        ans=arr[0];
-        for (int i=1;i<n;i++)
+        sum = 0;
+        ans = INT32_MIN;
+        int i = 0, j = 0;
+        while (j < n)
         {
-            if (abs(arr[i]%2) == abs(arr[i-1]%2))
+            if (sum < 0)
             {
-                ans=max(arr[i],sum);
-                x=arr[i];
+                sum = 0;
+                i = j;
             }
-            else if ((sum+arr[i])<arr[i])
+            if (i < j)
             {
-                ans=max(arr[i],sum);
-                x=arr[i];
+                if ((arr[j] ^ arr[j - 1]) & 1)
+                {
+                    sum += arr[j];
+                }
+                else
+                {
+                    i = j;
+                    sum = arr[j];
+                }
             }
-            else 
+            else
             {
-                sum+=arr[i];
-                ans=max(sum,x);
+                sum = arr[j];
             }
+            ans = max(ans, sum);
+            j++;
         }
-        cout<<ans<<endl;
+        cout << ans << endl;
     }
     return 0;
 }
