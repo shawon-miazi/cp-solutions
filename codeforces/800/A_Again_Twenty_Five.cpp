ddd7109816ae329efac8190ab6ac485b14c8bc25
shawon miazi <shawonmiazi655@gmail.com>
2026-06-04 23:55:55 +0600

    greedy

---
+++ codeforces/800/A_Again_Twenty_Five.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n,cnt=0;
+    cin>>n;
+    cnt=pow(5,n);
+    if (n<2)
+    cout<<cnt<<endl;
+    else 
+    cout<<25<<endl;
+}
